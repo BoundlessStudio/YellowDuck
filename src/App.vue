e0d4d6a2a9171d6a56def795a60c6c823049edab
RGBKnights <master@rgbknights.com>
2025-01-06 14:12:27 -0500

    testing 2 panels

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-import Card from './components/Card.vue'
+import Header from './components/HeaderSection.vue';
+import InputSection from './components/InputSection.vue';
+import InstructionsInput from './components/InstructionsSection.vue';
+import ActionSection from './components/ActionSection.vue';
+import OutputSection from './components/OutputSection.vue'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <main class="flex flex-row min-h-screen p-5 bg-cover bg-no-repeat bg-center" style="background-image: url('https://i.imgur.com/9yt5WWI.jpg');">
-    <section class="basis-1/3 flex items-center justify-center">
-      <Card />
+  <main class="flex flex-row min-h-screen p-5 bg-no-repeat bg-top " style="background-size: 100%; background-image: url('https://i.imgur.com/2xhsiql.jpg');">
+    <section class="basis-1/3 flex items-stretch justify-center p-6">
+      <div class="w-full h-fit shadow-2xl border-4 border-zinc-900 rounded">
+        <div class="h-fit bg-slate-50 p-8 text-gray-900">
+          <Header />
+          <InputSection />
+          <InstructionsInput />
+          <ActionSection />
+        </div>
+      </div>
     </section>
-    <section class="basis-2/3"></section>
+    <section class="basis-2/3 flex items-stretch justify-center p-6">
+      <OutputSection />
+    </section>
   </main>
   <Footer />
 </template>
--- src/components/ActionSection.vue
+++ src/components/ActionSection.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import ProgressBar from './ProgressBar.vue';
 
 const progress = ref(0);
+const isDone = ref(false);
 const isRunning = ref(false);
 let intervalId: number | null = null;
 
@@ -10,6 +11,7 @@ const loop = () => {
   intervalId = setInterval(() => {
     progress.value += 10;
     if(progress.value >= 100) {
+      isDone.value = true;
       clearLoop();
     }
   }, 200);
@@ -32,6 +34,12 @@ const stopLoop = () => {
   isRunning.value = false;
   clearLoop();
 };
+
+const resetLoop = () => {
+  isRunning.value = false;
+  isDone.value = false;
+  progress.value = 0;
+};
 </script>
 
 <template>
@@ -39,25 +47,24 @@ const stopLoop = () => {
     <h3 class="text-lg font-semibold">Iterate</h3>
     <span class="text-xs">Control the enumeration.</span>
     <div class="py-4">
-      <div v-if="isRunning" class="w-full">
-          <button
-            class="py-2 px-6 w-full rounded transition-colors inline-flex items-center justify-center bg-red-600 hover:bg-red-700 text-white"
-            @click="stopLoop"
-          >
-            Stop
-          </button>
-        </div>
-        <div v-else class="w-full">
-          <button 
-            class="py-2 px-6 w-full rounded transition-colors inline-flex items-center justify-center bg-green-600 hover:bg-green-700 text-white"
-            @click="startLoop"
-          >
-            Start
-          </button>
-        </div>
+      <div v-if="isDone">
+        <button @click="resetLoop" class="py-2 px-6 w-full rounded transition-colors inline-flex items-center justify-center bg-zinc-600 hover:bg-zinc-700 text-white">
+          Clear
+        </button>
       </div>
-      <div class="py-2">
-        <ProgressBar :progress="progress" :is-running="isRunning"/>
+      <div v-else-if="isRunning" class="w-full">
+        <button @click="stopLoop" class="py-2 px-6 w-full rounded transition-colors inline-flex items-center justify-center bg-red-600 hover:bg-red-700 text-white">
+          Stop
+        </button>
+      </div>
+      <div v-else class="w-full">
+        <button @click="startLoop" class="py-2 px-6 w-full rounded transition-colors inline-flex items-center justify-center bg-green-600 hover:bg-green-700 text-white">
+          Start
+        </button>
       </div>
     </div>
+    <div class="py-2">
+      <ProgressBar :progress="progress" :is-running="isRunning"/>
+    </div>
+  </div>
 </template>
